1b4238005abf102ec3cd9e8f190f38efd035ccf8
Will-8a <qwertywo361@gmail.com>
2024-12-01 04:25:22 -0400

    feat(report): add report list page structure

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,7 @@
   import Login from './auth/Login.svelte';
   import NewUser from './user/new_user.svelte';
   import NewStudent from './user/new_student.svelte';
+  import ReportsList from './report/report_list.svelte';
   import NewReport from './report/new_Report.svelte';
 
   const routes = {
@@ -12,6 +13,7 @@
     '/login': Login,
     '/tutors/new': NewUser,
     '/students/new': NewStudent,
+    '/reports': ReportsList,
     '/reports/new': NewReport
   };
 </script>
---
+++ src/report/report_list.svelte
@@ -0,0 +1,62 @@
+<script>
+  import AppLogo from '../assets/logo.svg';
+</script>
+
+<nav>
+  <input type='checkbox' id='check'/>
+
+  <!-- burger icon -->
+  <label for='check' class='checkbtn'>
+    <div class='burger'></div>
+    <div class='burger'></div>
+    <div class='burger'></div>
+  </label>
+
+  <img src={AppLogo} alt='logo' class='logo' />
+
+  <ul>
+    <li><a href="">Reportes</a></li>
+    <li><a href="">Contacto</a></li>
+    <li>
+      <a
+        href='/#/login'
+        role='button'
+      >
+        Cerrar sesión
+      </a>
+    </li>
+  </ul>
+</nav>
+
+<section>
+  <div class="hscroll">
+    <table>
+      <thead>
+        <tr class="cabecera">
+          <th>ID</th>
+          <th>N° Reporte</th>
+          <th>Cedula estudiante</th>
+          <th>Nombre estudiante</th>
+          <th>Carrera</th>
+          <th>Aprobación tutor</th>
+          <th>Aprobación Coordinador</th>
+          <!-- Read report button -->
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    </table>
+  </div>
+
+  <div class="padre-paginacion">
+    <div class="paginacion">
+      <a href="">&laquo;</a>
+      <a href="" class="activo">1</a>
+      <a href="">&raquo;</a>
+    </div>
+  </div>
+</section>
+
+<style>
+</style>
